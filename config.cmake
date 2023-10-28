bd5e41647b4c8e7b4b49eb5ef8e2fa0b84e3c59d
lihuizhao <1075452679@qq.com>
2023-10-28 10:39:01 +0800

    Solves some syntax problems that may be caused by versioning issues

--- config.cmake
+++ config.cmake
@@ -2,5 +2,5 @@ option(MD_USE_NEWTONS_THIRD_LAW_FLAG "Enable newton's third law" OFF)
 
 set(GPU_WAVEFRONT_SIZE "64" CACHE STRING "threads number in a wavefront (usually 32 for NVIDIA GPU or 64 for AMD GPU and DCU)")
 
-set(MD_KERNEL_STRATEGY "Default" CACHE STRING "kernel strategy")
+set(MD_KERNEL_STRATEGY "thread_atom" CACHE STRING "kernel strategy")
 set(MD_OPTIMIZE_OPTION "Default" CACHE STRING "optimization option")
